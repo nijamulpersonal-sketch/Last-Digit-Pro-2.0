3f2c03342565cb69c6cda07e8888c758e515b50c
Nijamul Mal <nijamul.personal@gmail.com>
2026-02-08 01:07:12 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,3 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-// PWA Service Worker Register
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/Last-Digit-Pro-2.0/sw.js");
-  });
-}
